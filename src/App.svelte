ff0082b6631a02545b3fcf20f7e5b4d1a0e60f21
holdena12 <102497243+holdena12@users.noreply.github.com>
2024-06-20 19:28:19 -0600

    added tones

--- src/App.svelte
+++ src/App.svelte
@@ -10,19 +10,25 @@
   let output = "";
   let set = 1;
   let interval;
+  let isWorking = "";
+  let workAudio;
+  let restAudio;
 
   function startInterval() {
     clearInterval(interval);
     interval = setInterval(() => {
       if (timeIsGoing) {
+        isWorking = "Working";
         time += 0.01;
         if (time >= setTime) {
           time = 0;
           restTimeIsGoing = true;
           timeIsGoing = false;
+          restAudio.play();
         }
       }
       if (restTimeIsGoing) {
+        isWorking = "Rest";
         restTime += 0.01;
         if (restTime >= setRest) {
           restTime = 0;
@@ -30,6 +36,7 @@
           if (set < setNumber) {
             timeIsGoing = true;
             set += 1;
+            workAudio.play();
           } else {
             clearInterval(interval);
           }
@@ -53,16 +60,20 @@
     startInterval();
   }
 </script>
+
 <div class="center">
-<p class="text">Set number: {set}</p>
-<p class="text">PTimer</p>
+  <p class="text">Set number: {set}</p>
+  <p class="text">PTimer</p>
 </div>
 <div class="center">
-<form on:submit|preventDefault={onStart}>
-  <input class="input" name="setNumber" placeholder="Number of sets" type="number">
-  <input  class="input" name="setTime" placeholder="Time of each set" type="number" step="0.01">
-  <input class="input" name="setRest" placeholder="Rest between sets" type="number" step="0.01">
-  <button class="button">Start</button>
-</form>
+  <form on:submit|preventDefault={onStart}>
+    <input class="input" name="setNumber" placeholder="Number of sets" type="number">
+    <input class="input"  name="setTime" placeholder="Time of each set" type="number" step="0.01">
+    <input class="input"  name="setRest" placeholder="Rest between sets" type="number" step="0.01">
+    <button class="button">Submit</button>
+  </form>
 </div>
-<p class="text" style="font-size: 30px;">{output}</p>
+<p class="text" style="font-size: 30px;">{isWorking} {output}</p>
+
+<audio bind:this={workAudio} src="alert2.wav"></audio>
+<audio bind:this={restAudio} src="alert.wav"></audio>
